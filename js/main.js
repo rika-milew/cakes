fe39a994ea214cbff8365a7bd673051b963e5641
Erika Gumerova <95300271+erikagumerova@users.noreply.github.com>
2022-08-22 14:20:43 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,309 @@
+cakes = [
+    {
+name: "Лаванда",
+image: "images/cake1.jpg",
+price: "19.99 р.",
+decription: "<p>Торт состоит из нескольких слоёв нежного масляного бисквита.</p><p>Слои соединены джемом из черной смородины и кремом, который приготовлен из сыра творожного, сметаны и безе с добавлением лавандового сиропа.</p><p>Украшен торт лепестками из шоколада и лаванды.</p>",
+},
+    {
+name: "Греческий флёр",
+image: "images/cake2.jpg",
+price: "19.99 р.",
+decription: "В основе тортика слои белого лёгкого бисквита, пропитанные коньяком. Слои бисквита покрыты легким кремом на основе йогурта «Греческий» и натуральных сливок. Внутри слои густого и в меру сладкого конфитюра из красной смородины.",
+},
+    {
+name: "Красный бархат",
+ image: "images/cake3.jpg",
+ price: "20.99 р.",
+decription: "Красный бархат - сочетание роскошного бархатного бисквита и нежного крема на основе маскарпоне, торт прослоен самосваренным джемом с кусочками клубники, а бисквит очень мягкий и увлажненный.",
+},
+    {
+name: "Чизберри",
+image: "images/cake4.jpg",
+price: "24.99 р.",
+decription: "В основе тортика слои белого лёгкого бисквита, пропитанные коньяком. Бисквит словно окутан воздушным кремом из творожного сыра и натуральных сливок, смешанные с ягодным конфитюром из малины и черники. Внутри кроются цельносваренные ягоды – малиновый и черничный слои. Вкус крема многогранен: слегка солоноватый творожный сыр дополнен в меру сладким вкусом натуральных спелых ягод малины и черники",
+},
+    {
+name: "Творожно-Малиновый",
+image: "images/cake5.jpg",
+price: "24.99 р.",
+decription: "Песочно-марципановый корж, прослоенный нежным сметанно-творожным кремом с добавлением ягод малины. Торт украшен ягодами малины.",
+},
+    {
+name: "Молочная девочка",
+image: "images/cake6.jpg",
+price: "21.99 р.",
+decription: "Несколько слоев воздушного бисквита на основе сгущённого молока прослоены кремом из натуральных сливок с добавлением ягод малины. Декор торта выполнен из свежих ягод голубики и веточек розмарина.",
+},
+    {
+name: "Творожно-Черничный",
+image: "images/cake7.jpg",
+price: "25.99 р.",
+decription: "Песочно-марципановый корж, прослоенный нежным сметанно-творожным кремом с добавлением ягод черники. Торт украшен ягодами черники.",
+},
+    {
+name: "Трюфель",
+image: "images/cake8.jpg",
+price: "25.99 р.",
+decription: "Несколько слоёв шоколадно -ромового бисквита пропитанных сиропом сахарным с коньяком и прослоенных кремом заварным с добавлением какао-порошка, коньяка и масла сливочного.",
+},
+    {
+name: "Торт с сыром маскарпоне",
+image: "images/cake9.jpg",
+price: "22.99 р.",
+decription: "Нежный бисквит пропитан сиропом и прослоен кремом с сыром Маскарпоне с добавлением апельсинового сока, украшен фруктами или ягодами в желе.",
+},
+    {
+name: "Сникерс",
+image: "images/cake10.jpg",
+price: "25.99 р.",
+decription: "Два слоя бисквита с добавлением какао-порошка, не пропитанные сиропом, один воздушно ореховый слой, прослоены кремом на основе сливочного масла и молока сгущённого вареного. Внутри прослойка из солёной карамели с цельным арахисом. Поверхность оформлена ганашом на основе натурального молочного шоколада, карамелью, цельным арахисом и батончиком «Сникерс».",
+},
+    {
+name: "Ягодная поляна",
+image: "images/cake11.jpg",
+price: "18.99 р.",
+decription: "Несколько слоёв нежного бисквита со шпинатом, пропитанные сметанным кремом, украшен клюквой и веточками розмарина.",
+},
+    {
+name: "Торт тирамису",
+image: "images/cake12.jpg",
+price: "21.99 р.",
+decription: "Слой нежного бисквита, пропитан кофейным сиропом и прослоен кремом с сыром тирамису. Внутри торта слои итальянского бисквитного печенья (палочки «савоярди»), пропитанные кофейным сиропом и прослоены кремом с сыром. Боковая поверхность украшена палочками «савоярди». Сверху торт посыпан какао-порошком.",
+},
+    {
+name: "Киевский с фундуком",
+image: "images/cake13.jpg",
+price: "26.99 р.",
+decription: "Слои хрустящих воздушно-ореховых коржей соединены между собой кремом на основе сливочного масла. Поверхность заглазирована.",
+},
+    {
+name: "Медовая нежность",
+image: "images/cake14.jpg",
+price: "16.49 р.",
+decription: "Слои бисквитно-медовых коржей пропитаны сиропом и соединены между собой нежным сметанным кремом с фруктами консервированными.",
+},
+    {
+name: "Марсала",
+image: "images/cake15.jpg",
+price: "22.99 р.",
+decription: "Несколько слоёв выпеченного кофейного полуфабриката, прослоенные шоколадным кремом с сыром маскарпоне и малиновым желе.",
+},
+    {
+name: "Лимонное",
+image: "images/cake16.jpg",
+price: "22.99 р.",
+decription: "Порционное изделие состоит из молочного бисквита, пропитанного сахарным сиропом с коньяком. В прослойке крем из сгущенного молока с маслом сливочным с добавлением свежих лимонов.",
+},
+    {
+name: "Морковное Английское",
+image: "images/cake17.jpg",
+price: "21.99 р.",
+decription: "Морковные коржи прослоенные сырным кремом . Торт украшен зеркальной глазурью и грецким орехом. Боковая поверхность обсыпана слайсами миндального ореха.",
+},
+    {
+name: "Ягодное суфле",
+image: "images/cake18.jpg",
+price: "18.99 р.",
+decription: "Песочный корж на масле сливочном, прослоен ягодным суфле, сверху залит шоколадной глазурью и украшен белковым кремом.",
+},
+]
+
+let s = "";
+
+for (i=0; i<cakes.length; i++) {
+s += `
+<div class="card" onclick="showCard('${cakes[i].name}','${cakes[i].price}','${cakes[i].decription}','${cakes[i].image}')">
+<img src=${cakes[i].image} alt="" class="menu-image">
+<h3>${cakes[i].name}</h3>
+<p class="price">${cakes[i].price}</p>
+</div>
+`;
+}
+
+cards.innerHTML = s;
+
+
+cheesecakes = [
+    {
+name: "Ягодный",
+image: "images/cheesecake1.jpg",
+price: "25.29 р.",
+decription: "На песочном корже, изготовленном на масле сливочном выпечен нежный творожный слой. Который украшен ягодами залитыми желе.",
+},
+    {
+name: "Ванильный",
+image: "images/cheesecake2.jpg",
+price: "25.99 р.",
+decription: "Один песочный слой на основе сливочного масла, пропитанный сиропом. Кремовая основа состоит из творожного срыва, творога, сметаны и белого шоколада. Верхняя часть чизкейка покрыта зеркальной глазурью, украшена свежими ягодами голубики и зелени.",
+},
+    {
+name: "Шоколадный",
+ image: "images/cheesecake3.jpg",
+ price: "25.99 р.",
+decription: "Нижний слой чизкейка изготовлен из шоколадного бисквита с добавлением масла сливочного. В основе начинки для чизкейка сыр рикотта, сметана с добавлением яиц и шоколада.",
+},
+    {
+name: "Фисташковый",
+image: "images/cheesecake4.jpg",
+price: "29.99 р.",
+decription: "Один песочный слой с добавлением сливочного масла, не пропитанный сиропом, покрыт сметанно-творожным кремом с добавлением фисташковой пасты. Поверхность оформлена фисташковой пастой, глазурью белой и очищенными фисташками.",
+},
+{
+name: "Набор чизкейков",
+image: "images/cheesecake5.jpg",
+price: "18.49 р.",
+decription: "Набор состоит из сегментов разных чизкейков: ванильный, фисташковый, шоколадный и ягодный.",
+},
+    {
+name: "Сегмент чизкейк",
+image: "images/cheesecake6.jpg",
+price: "4.59 р.",
+decription: "Кусочек чизкейка сразу упакован в удобный контейнер с вилочкой. На выбор предлагаем классические варианты: ванильный и шоколадный.",
+},
+]
+
+let p = "";
+
+for (n=0; n<cheesecakes.length; n++) {
+p += `
+<div class="card" onclick="showCheesecakeCard('${cheesecakes[n].name}','${cheesecakes[n].price}','${cheesecakes[n].decription}','${cheesecakes[n].image}')">
+<img src=${cheesecakes[n].image} alt="" class="menu-image">
+<h3>${cheesecakes[n].name}</h3>
+<p class="price">${cheesecakes[n].price}</p>
+</div>
+`;
+}
+
+cheeseCards.innerHTML = p;
+
+eclairs = [
+    {
+name: "Эклер Сливочный",
+image: "images/eclair1.jpg",
+price: "4.49 р.",
+decription: "Ванильный эклер из заварного теста наполнен масляным кремом на основе сливочного масла с добавлением заварного крема, верхняя поверхность покрыта помадкой сахарной белой.",
+},
+    {
+name: "Эклер со сгущенкой",
+image: "images/eclair2.jpg",
+price: "4.49 р.",
+decription: "Ванильный эклер из заварного теста наполнен сливочным кремом с добавлением вареного сгущённого молока, верхняя поверхность покрыта шоколадной помадкой.",
+},
+    {
+name: "Профитроль творожный",
+ image: "images/eclair3.jpg",
+ price: "4.49 р.",
+decription: "Ванильный профитроль из заварного теста наполнен творожным кремом, верхняя поверхность посыпана пудрой сахарной.",
+},
+]
+
+let o = "";
+
+for (m=0; m<eclairs.length; m++) {
+o += `
+<div class="card" onclick="showEclairCard('${eclairs[m].name}','${eclairs[m].price}','${eclairs[m].decription}','${eclairs[m].image}')">
+<img src=${eclairs[m].image} alt="" class="menu-image">
+<h3>${eclairs[m].name}</h3>
+<p class="price">${eclairs[m].price}</p>
+</div>
+`;
+}
+
+eclairsCards.innerHTML = o;
+
+
+let content="";
+content = document.getElementById('wrapper');
+
+let newCard="";
+newCard = document.getElementById('fullCard');
+
+
+function showCard(cakeName, cakePrice, cakeDescription, cakeImage) {
+    // newFullCard = document.getElementById(`full-item-card${cakeId}`);
+    // console.log(newFullCard); 
+    newCard.innerHTML = `
+    <div class="full-card">
+    <img src="images/close.png" alt="" class="closeImage" onclick="closeCard()">
+    <div>
+   <img src="${cakeImage}" alt="" class="full-card__image" onclick="showImage()">
+   </div>
+   <div class="full-card__text">
+   <p>${cakeName}</p>
+   <p>${cakePrice}</p>
+   <p>${cakeDescription}</p>
+   </div>
+   </div>
+   `;
+// document.body.classList.add("windowOpened");
+    newCard.classList.add("card-opened");
+//    content.classList.toggle("window-opened");
+// cards.classList.add("full-card-opened");
+   
+  }
+
+  function closeCard() {
+    // content.style.display = 'block';
+    // content.classList.toggle("window-opened");
+    // newCard.style.display = 'none';
+    newCard.classList.remove("card-opened");
+    // content.style.opacity = '1';
+    
+// content.classList.remove("window-opened");
+  }
+
+  function showCheesecakeCard(cheesecakeName, cheesecakePrice, cheesecakeDescription, cheesecakeImage) {
+    
+    newCard.innerHTML = `
+    <div class="full-card">
+    <img src="images/close.png" alt="" class="closeImage" onclick="closeCard()">
+    <div>
+   <img src="${cheesecakeImage}" alt="" class="full-card__image">
+   </div>
+   <div class="full-card__text">
+   <p>${cheesecakeName}</p>
+   <p>${cheesecakePrice}</p>
+   <p>${cheesecakeDescription}</p>
+   </div>
+   </div>
+   `;
+    newCard.classList.add("card-opened");
+  }
+
+  function showEclairCard(eclairName, eclairPrice, eclairDescription, eclairImage) {
+   newCard.innerHTML = `
+    <div class="full-card">
+    <img src="images/close.png" alt="" class="closeImage" onclick="closeCard()">
+    <div>
+   <img src="${eclairImage}" alt="" class="full-card__image">
+   </div>
+   <div class="full-card__text">
+   <p>${eclairName}</p>
+   <p>${eclairPrice}</p>
+   <p>${eclairDescription}</p>
+   </div>
+   </div>
+   `;
+    newCard.classList.add("card-opened");
+    }
+
+
+    let newImageCard="";
+    newImageCard = document.getElementById('fullImageCard');
+
+let fullImage = "";
+   
+
+    function showImage() {
+        fullImage = document.getElementsByClassName('full-card__image');
+        // newFullCard = document.getElementById(`full-item-card${cakeId}`);
+        // console.log(newFullCard); 
+    //     newImageCard.innerHTML = `
+    //     <div class="full-image-card">
+    //    <img src="${cakeImage}" alt="" class="full-card__big-image">
+    //    </div>
+    //    `;
+    //    newImageCard.classList.add("full-image-card-opened");
+    fullImage.classList.add("full-image-card-opened");
+      }
